e98bab0687aaaeddef15e5b760384e1602d8f6e3
Santi.pdr <santiagopereiraxd029@gmail.com>
2026-09-25 15:06:01 -0300

    fix: preserve legacy loot scatter for TRex

--- src/main/java/com/santipdr/copyl/common/entity/TRexEntity.java
+++ src/main/java/com/santipdr/copyl/common/entity/TRexEntity.java
@@ -1,5 +1,7 @@
 package com.santipdr.copyl.common.entity;
 
+import com.santipdr.copyl.common.util.LegacyRandom;
+
 import com.santipdr.copyl.common.entity.ai.LongRangeWanderGoal;
 import com.santipdr.copyl.common.item.ModItems;
 import com.santipdr.copyl.common.item.material.ModMaterialItems;
@@ -207,9 +209,9 @@ public final class TRexEntity extends Monster {
     private void dropItemRand(Item item, int count) {
         this.level().addFreshEntity(new ItemEntity(
                 this.level(),
-                this.getX() + this.random.nextInt(4) - this.random.nextInt(4),
+                this.getX() + LegacyRandom.nextInt(4) - LegacyRandom.nextInt(4),
                 this.getY() + 1.0D,
-                this.getZ() + this.random.nextInt(4) - this.random.nextInt(4),
+                this.getZ() + LegacyRandom.nextInt(4) - LegacyRandom.nextInt(4),
                 new ItemStack(item, count)));
     }
 
