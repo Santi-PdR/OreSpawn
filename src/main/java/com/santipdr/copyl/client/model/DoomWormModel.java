6668142cbefd8252d7da44d3c5991c1171858fa6
Santi.pdr <santiagopereiraxd029@gmail.com>
2026-09-24 21:58:41 -0300

    fix: correct Doom Worm model import

--- src/main/java/com/santipdr/copyl/client/model/DoomWormModel.java
+++ src/main/java/com/santipdr/copyl/client/model/DoomWormModel.java
@@ -7,7 +7,7 @@ import com.santipdr.copyl.common.entity.DoomWormEntity;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.ModelPart;
-import net.minecraft.client.model.PartPose;
+import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.builders.*;
 import net.minecraft.resources.ResourceLocation;
 
