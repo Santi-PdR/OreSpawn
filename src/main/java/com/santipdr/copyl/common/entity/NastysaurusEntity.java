30319997d2fbe41c8babafa03ca328ecf0187f0b
Santi.pdr <santiagopereiraxd029@gmail.com>
2026-09-25 15:05:47 -0300

    fix: preserve legacy loot scatter for Nastysaurus

--- src/main/java/com/santipdr/copyl/common/entity/NastysaurusEntity.java
+++ src/main/java/com/santipdr/copyl/common/entity/NastysaurusEntity.java
@@ -1,5 +1,7 @@
 package com.santipdr.copyl.common.entity;
 
+import com.santipdr.copyl.common.util.LegacyRandom;
+
 import com.santipdr.copyl.common.entity.ai.LongRangeWanderGoal;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.syncher.EntityDataAccessor;
@@ -183,8 +185,8 @@ public final class NastysaurusEntity extends Monster {
     private void dropItemRand(Item item, int count) {
         if (this.level().isClientSide) return;
         for (int i = 0; i < count; i++) {
-            double x = this.getX() + this.random.nextInt(4) - this.random.nextInt(4);
-            double z = this.getZ() + this.random.nextInt(4) - this.random.nextInt(4);
+            double x = this.getX() + LegacyRandom.nextInt(4) - LegacyRandom.nextInt(4);
+            double z = this.getZ() + LegacyRandom.nextInt(4) - LegacyRandom.nextInt(4);
             ItemEntity entity = new ItemEntity(this.level(), x, this.getY() + 3.0D, z, new ItemStack(item));
             this.level().addFreshEntity(entity);
         }
