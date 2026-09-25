ed567a3d1e4d36e41d41d696de2e575d302aefb1
Santi.pdr <santiagopereiraxd029@gmail.com>
2026-09-24 23:25:21 -0300

    fix: import server level API for spyro clone

--- src/main/java/com/santipdr/copyl/common/entity/SpyroEntity.java
+++ src/main/java/com/santipdr/copyl/common/entity/SpyroEntity.java
@@ -33,6 +33,7 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.util.RandomSource;
