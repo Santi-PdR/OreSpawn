8f3eee3478238bd17ba7bcb1674d3feccab963f9
Santi.pdr <santiagopereiraxd029@gmail.com>
2026-09-25 15:05:29 -0300

    fix: preserve legacy loot scatter for Alosaurus

--- src/main/java/com/santipdr/copyl/common/entity/AlosaurusEntity.java
+++ src/main/java/com/santipdr/copyl/common/entity/AlosaurusEntity.java
@@ -1,5 +1,7 @@
 package com.santipdr.copyl.common.entity;
 
+import com.santipdr.copyl.common.util.LegacyRandom;
+
 import com.santipdr.copyl.common.entity.ai.LongRangeWanderGoal;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.syncher.EntityDataAccessor;
@@ -197,9 +199,9 @@ public final class AlosaurusEntity extends Monster {
     private void dropItemRand(Item item, int count) {
         ItemEntity dropped = new ItemEntity(
                 this.level(),
-                this.getX() + this.random.nextInt(4) - this.random.nextInt(4),
+                this.getX() + LegacyRandom.nextInt(4) - LegacyRandom.nextInt(4),
                 this.getY() + 1.0D,
-                this.getZ() + this.random.nextInt(4) - this.random.nextInt(4),
+                this.getZ() + LegacyRandom.nextInt(4) - LegacyRandom.nextInt(4),
                 new ItemStack(item, count)
         );
         this.level().addFreshEntity(dropped);
